8b60f34431b0062e3931db374528f914de355557
Catherine Dinh <dinhcaatherine@gmail.com>
2021-02-18 00:20:21 -0800

    alt changes

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,4 +10,9 @@ $(document).ready(function() {
  */
 function initializePage() {
 	// your code here
+	$(".likeBtn").click(clickLike);
 }
+
+function clickLike() {
+	ga("send", "event", "like", "click");
+};
